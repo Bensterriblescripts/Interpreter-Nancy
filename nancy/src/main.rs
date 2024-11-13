5933307c55fc77deef98d53c200e0927ed65b174
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-11-13 23:32:49 +1300

    idk why im doing this

---
+++ nancy/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let path = "C:\\Repositories\\Interpreter-Nancy\\nancy\\test.na";
+    let newline;
+    let text;
+
+    // Extract Text
+    match open_file(path) {
+        Ok(contents) => {
+            text = contents;
+            println!("{}", text);
+        },
+        Err(error) => println!("Error opening file: {}", error),
+    }
+}
+fn open_file(path: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
