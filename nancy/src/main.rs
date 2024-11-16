3b6b84a7c6b639f95ae5e4eec6acc4a6aee37854
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-11-16 17:51:19 +1300

    cfh

--- nancy/src/main.rs
+++ nancy/src/main.rs
@@ -54,6 +54,9 @@ fn main() -> Result<(), Error> {
     let mut variables: Vec<Variable> = Vec::new();
     let mut conditions: Vec<Condition> = Vec::new();
 
+    // Compile Regex - Blocks
+    let re_program = Regex::new(r"new\s*(?<program>\w+)\s*-\s*(?<programtype>\w+)")?;
+
     // Compile Regex - Single Word Types
     let re_string = Regex::new(r#"\s*"(?<string>.*)""#)?;
     let re_variable = Regex::new(r"\s*(?<varible>\w+)")?;
@@ -62,23 +65,20 @@ fn main() -> Result<(), Error> {
     let re_int = Regex::new(r"\s*(?<int>\d+)")?;
 
     // Compile Regex - Variable Declarations
-    let re_assignvar = Regex::new(r"let|have")?; // have (assigning a variable)
+    let re_assignvar = Regex::new(r"(?<variable>\w+)\s*(equals|=|==|is)(?<value>\w+)\s*(\d+|\d+.\d+|true|false|.*)")?; // have (assigning a variable)
     let re_allocatearr = Regex::new(r"contain|store|contains|stores")?; // contain (assigning an array)
     // Compile Regex - Assignment Types
     let re_assignbool = Regex::new(r"(?i)\s*(?<name>\w+)\s*is\s*(?<bool>true|false)")?; // Any word equal to true or false
     let re_assignint = Regex::new(r"\s*(?<name>\w+)\s*is\s*(?<int>\d+)$")?; // Any number (excludes digits)
-    let re_assignfloat = Regex::new(r"\s*(?<name>\w+)\s*is\s*(?<float>(?:0|[1-9]\d*)\.\d+)")?; // Any number with digits
+    let re_assignfloat = Regex::new(r"\s*(?<name>\w+)\s*is\s*(?<float>(?:0|[1-9]\d*)\.\d +)")?; // Any number with digits
     let re_assignstring = Regex::new(r#"\s*(?<name>\w+)\s*is\s*"(?<string>.*)""#)?; // Any collection of characters
 
     // Compile Regex - Conditions
-    let re_ifequals = Regex::new(r"(?i)\s.*(if|else if)\s*(?<left>\d+|\d+.\d+|true|false|.*)\s*(is|equals)\s*(?<right>\d+|\d+.\d+|true|false|.*)")?; // equals (conditional match)
-    let re_ifnotequals = Regex::new(r"does not equal")?; // does not equal (conditional negative match)
+    let re_if = Regex::new(r"(?i)\s.*(if|else if|is|or is)\s*(?<left>\d+|\d+.\d+|true|false|\w+)\s*(?<operator>(is|equals|==|===|!=|!==|greater than|less than|greater than or equal to|less than or equal to|>=|>|<=|<))\s*(?<right>\d+|\d+.\d+|true|false|.*)")?; // equals (conditional match)
 
     // Compile Regex - Loops
-    let re_loopwhilecond = Regex::new(r"\s*while(?<left>\d+|\d+.\d+|true|false|.*)\s*(?<operator>>|>=|<|<=|=|==|===)\s*(?<right>\d+|\d+.\d+|true|false|.*)");
-    let re_loopwhiletrue = Regex::new(r"\s*while(?<var>\w+)");
-    let re_loopint = Regex::new(r"\s*while(?<iter>\d+)")?;
-    let re_loopintfunction = Regex::new(r"\s*run\s*(?<function>\w+)\s*(?<iter>\d+)\s*times"); // run function 15 times
+    let re_loopwhile = Regex::new(r"\s*while(?<left>\d+|\d+.\d+|true|false|.*)\s*(?<operator>>|>=|<|<=|=|==|===|is|is not)\s*(?<right>\d+|\d+.\d+|true|false|.*)");
+    let re_looprun = Regex::new(r"\s*run\s*(?<function>\w+)\s*(?<iter>\d+)\s*times"); // run function 15 times
 
     // TODO: Convert document to utf8
 
@@ -97,8 +97,8 @@ fn main() -> Result<(), Error> {
     let mut linenumber: i32 = 1;
     for line in text.lines() {
 
-        // 0 - Comment, 1 - Variable, 2 - Condition, 3 - Loop, 4 - Function
-        let linetype: i8;
+        // 0 - Comment, 1 - Variable, 2 - Condition, 3 - Loop, 4 - Function, 10 - Program
+        let mut linetype: i8 = -1;
 
         // First Character
         for first in line.chars() {
@@ -107,28 +107,31 @@ fn main() -> Result<(), Error> {
                 linetype = 0;
                 break;
             }
+            if first == 'n' { // Program
+                if let Some(caps) = re_program.captures(line) {
+                    new_program(caps, linenumber);
+                    break;
+                }
+            }
             if first == 'i' { // Condition
-                if let Some(caps) = re_ifequals.captures(line) {
-                    linetype = 2;
+                if let Some(caps) = re_if.captures(line) {
+                    new_condition(caps, linenumber);
                     break;
                 }
-                else if let Some(caps) = re_ifnotequals.captures(line) {
-                    linetype = 2;
-                    break;
-                }
-                else {
-                    // if is_variable(first) {
-                    //     linetype = 1;
-                    //     break;
-                    // }
+            }
+            if first == 'w' || first == 'r' { // Loop (while/run) {
+                if let Some(caps) = re_loopwhile.captures(line) {
+                    new_loop(caps, linenumber);
                 }
             }
-            if first == 'l' || first == 'h' { // Variable (let/have)
 
-            }
-            if first == 'w' || first == 'r' { // Loop (while/run)
+            // Check variables
+        }
+        linenumber += 1;
 
-            }
+        // End on irrelevant
+        if linetype == -1 || linetype == 0 {
+            continue;
         }
 
         /* Variable */
@@ -154,115 +157,6 @@ fn main() -> Result<(), Error> {
     //                 });
     //             }
     //         }
-    //         else if let Some(caps) = re_assignint.captures(line) {
-    //             println!("Found an integer declaration on line {}", linenumber);
-    //             variables.push(Variable {
-    //                 name: String::from(&caps["name"]),
-    //                 var_type: String::from("Integer"),
-    //                 scope: 0,
-    //                 data: Type::Integer(caps["int"].parse().unwrap()),
-    //             });
-    //         }
-    //         else if let Some(caps) = re_assignfloat.captures(line) {
-    //             println!("Found a float declaration on line {}", linenumber);
-    //             variables.push(Variable {
-    //                 name: String::from(&caps["name"]),
-    //                 var_type: String::from("Float"),
-    //                 scope: 0,
-    //                 data: Type::Float(caps["float"].parse().unwrap()),
-    //             });
-    //         }
-    //         else if let Some(caps) = re_assignstring.captures(line) {
-    //             println!("Found a string declaration on line {}", linenumber);
-    //             variables.push(Variable {
-    //                 name: String::from(&caps["name"]),
-    //                 var_type: String::from("String"),
-    //                 scope: 0,
-    //                 data: Type::String(String::from(&caps["string"])),
-    //             });
-    //         }
-    //     }
-    //     else if re_are.is_match(line) {
-    //         println!("Found an array declaration on line {}", linenumber);
-    //     }
-    //
-    //
-    //     /* Condition */
-    //
-    //     // Equals
-    //     if let Some(caps) = re_ifequals.captures(line).or_else(|| re_ifequals.captures(line)) {
-    //         println!("Identified condition on line {}\nLeft variable, {}, Right variable: {}", linenumber, &caps["left"], &caps["right"]);
-    //
-    //         // 0 - Var, 1 - Bool, 2 - Int, 3 - Float, 4 - String
-    //         let mut lefttype: i8 = 0;
-    //         let mut righttype: i8 = 0;
-    //
-    //         // Left Argument
-    //         if let Some(caps) = re_string.captures(&caps["left"]) {
-    //             println!("Left assignment is a string");
-    //             lefttype = 4;
-    //         }
-    //         else if let Some(caps) = re_variable.captures(&caps["left"]) {
-    //             println!("Left assignment is a variable name");
-    //             // Iterate through the existing variables to check if it exists
-    //         }
-    //         else if let Some(caps) = re_bool.captures(&caps["left"]) {
-    //             println!("Left assignment is a bool");
-    //             lefttype = 1
-    //         }
-    //
-    //         // Right Argument
-    //         if let Some(caps) = re_string.captures(&caps["right"]) {
-    //             println!("Right assignment is a string");
-    //             righttype = 4;
-    //         }
-    //         else if let Some(caps) = re_variable.captures(&caps["right"]) {
-    //             println!("Right assignment is a variable name");
-    //             // Iterate through the existing variables to check if it exists
-    //         }
-    //         else if let Some(caps) = re_bool.captures(&caps["right"]) {
-    //             println!("Right assignment is a bool");
-    //             righttype = 1
-    //         }
-    //
-    //         // Only String-Int/Floats Can Mismatch
-    //         if lefttype != righttype {
-    //
-    //         }
-    //
-    //
-    //
-    //         let leftvar: &Variable;
-    //         let rightvar: &Variable;
-    //         //
-    //         // // Left Variable Exists
-    //         // if let Some(index) = variables.iter().position(|v| v.name.contains(caps["left"].parse().unwrap())) {
-    //         //     leftvar = &variables[index];
-    //         //     println!("The left variable exists")
-    //         // }
-    //         // // Right Variable Exists
-    //         // if let Some(index) = variables.iter().position(|v| v.name.contains(caps["right"].parse().unwrap())) {
-    //         //     rightvar = &variables[index];
-    //         //     println!("The right variable exists");
-    //     }
-    //
-    //         // conditions.push(Condition {
-    //         //     position: 0,
-    //         //     left: caps["left"].parse().unwrap(),
-    //         //     right: ,
-    //         //     condition_type:
-    //         // });
-    //     // Not Equal
-    //     else if let Some(caps) = re_ifequals.captures(line) {
-    //         println!("Found an if-equals on line {}", linenumber);
-    //     }
-    //
-    //     linenumber += 1;
-    // }
-    //
-    // println!("\n");
-    // for variable in variables {
-    //     println!("Variable: {:?}", variable);
     }
 
     Ok(())
@@ -274,10 +168,13 @@ fn open_file(path: &str) -> Result<String, std::io::Error> {
     Ok(contents)
 }
 
-fn handle_if() {
-
+fn new_program(line: regex::Captures, linenumber: i32) {
+    println!("Created a new program on line: {}\nProgram Type: {}, Program Name: {}\n", linenumber, &line["program"], &line["programtype"]);
 }
-fn handle_loop() {
+fn new_condition(line: regex::Captures, linenumber: i32) {
+    println!("Found a condition on line: {}\nLeft Variable: {}, Right Variable: {}, Operator: {}\n", linenumber, &line["left"], &line["right"], &line["operator"]);
+}
+fn new_loop(line: regex::Captures, linenumber: i32) {
 
 }
 
